315c5492c3b649c10672f8e31d600e17f4d94fdf
Kekevaa <matiaskerttula@gmail.com>
2024-11-20 11:47:50 +0200

    Hakusysteemi v2.2

--- src/App.js
+++ src/App.js
@@ -1,49 +1,4 @@
-/*
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';  
-import Search from './components/search';
-import Naytokset from './components/naytokset'
 
- 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {     }
-        <Route
-          path="/"
-          element={
-            <div className="App">
-              <header className="App-header">
-                <h1>The "best" movie page</h1>
-              </header>
-              
-              <section className="App-section">
-                <h2>Browse movies or see showings from Finnkino.</h2>
-                <Link to="/Naytokset">
-                  <button className="section-button">Search showings</button>
-                </Link>
-                <Link to="/Search">
-                  <button className="section-button">Search movies</button>
-                </Link>
-              </section>
-            </div>
-          }
-        />
- 
-        <Route path="/naytokset" element={<Naytokset/>} />
-        <Route path="/search" element={<Search/>} />
- 
-      </Routes>
-    </Router>
-  );
-}
- 
-export default App;
-
-
-*/
 
 import React from 'react';
 import './App.css';
